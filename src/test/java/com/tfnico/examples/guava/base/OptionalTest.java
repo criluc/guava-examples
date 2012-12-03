e58157751fd486d58bc87764809580f79eb98f63
Yongfeng LI <wliyongfeng@gamil.com>
2012-12-03 15:27:51 +0000

    add Optional test

---
+++ src/test/java/com/tfnico/examples/guava/base/OptionalTest.java
@@ -0,0 +1,49 @@
+package com.tfnico.examples.guava.base;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import com.google.common.base.*;
+import com.google.common.collect.Maps;
+
+public class OptionalTest {
+    private static Map<String, String> stateCapitals;
+
+    @Before
+    public void setUp() {
+        final Map<String, String> tempStatesToCapitals = Maps.newHashMap();
+        tempStatesToCapitals.put("Alaska", "Juneau");
+        tempStatesToCapitals.put("Arkansas", "Little Rock");
+        tempStatesToCapitals.put("Colorado", "Denver");
+        tempStatesToCapitals.put("Idaho", "Boise");
+        tempStatesToCapitals.put("Utah", "Salt Lake City");
+        tempStatesToCapitals.put("Wyoming", "Cheyenne");
+        stateCapitals = Collections.unmodifiableMap(tempStatesToCapitals);
+    }
+
+    @Test
+    public void testOptionalAbsent() {
+        assertEquals(Optional.fromNullable(stateCapitals.get("alaska")),
+                Optional.absent());
+        assertTrue(Optional.fromNullable(stateCapitals.get("Alaska"))
+                .isPresent());
+    }
+
+    @Test
+    public void testOptionalOr() {
+        Optional<String> optional = Optional.fromNullable(stateCapitals
+                .get("alaska"));
+        assertEquals(optional.or("empty"), "empty");
+
+        optional = Optional.fromNullable(stateCapitals.get("Alaska"));
+        assertEquals(optional.get(), "Juneau");
+        assertEquals(optional.or("empty"), "Juneau");
+        assertEquals(optional, Optional.of("Juneau"));
+        assertEquals(optional.getClass().getName(),
+                "com.google.common.base.Present");
+    }
+}
